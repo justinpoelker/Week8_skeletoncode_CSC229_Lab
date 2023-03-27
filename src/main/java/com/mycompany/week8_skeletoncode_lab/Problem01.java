813fdafe40aec603f07120834ad95468d96c582c
Justin <Justin@Justin-Main-PC>
2023-03-26 21:43:00 -0400

    Finished project and added comments.

--- src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
@@ -14,7 +14,48 @@ public class Problem01 {
     // Todo 04: Develop a method that returns the sum of the prime numbers between 1 and n
     //          Test your solution
     //          Analyze its space and time  
+    int[] arr = new int[n+1];
+    long[] sumPrime = new long[n+1];
     
+    for(int i = 2; i*i <= n; i++){
+        if(arr[i] == 0){
+            for(int hold = i*i; hold <= n; hold += i){
+                arr[hold] = 1;
+            }
+        }
+    }
+    
+    for(int i = 2; i <= n; i++){
+        if(arr[i] == 0){
+            sumPrime[i] = sumPrime[i-1] + 1;
+        }
+        else {
+            sumPrime[i] = sumPrime[i-1];
+        }
+    }
+    return sumPrime[n];
+    }
+    
+    //The time complexity of this algorithm is O(n log log n) which will grow slower depending on n
+    //Space complexity is O(n) 
+    
+    
+    public static void main(String[] args){
+        //Test case for n = 10
+        //Sum of primes between 1 and 10
+        long expected1 = 17; 
+        
+        //Call the sumOfPrimes method
+        long actual1 = getSumOfPrimes(10);
+        assert expected1 == actual1 : "Numbers not equal for test 1";
+        
+        
+        //Case 2, same logic applies
+        long expected2 = 77;
+        long actual2 = getSumOfPrimes(20);
+        assert expected2 == actual2 : "Numbers not equal for test 2";
+        
+        System.out.println("All numbers equal to the sum of prime");
     }
     
 }
